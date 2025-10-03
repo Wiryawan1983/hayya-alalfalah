9519c3231312cb5f7ac3a8b5ac14cdf538068ebc
Wiryawan1983 <121319884+Wiryawan1983@users.noreply.github.com>
2025-10-03 14:47:23 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
-const repoBase = "/hayya-alalfalah"; // path repo GitHub Pages
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={repoBase}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
